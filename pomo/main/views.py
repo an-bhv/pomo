a6041ec7912a72d55236587bda6125a644d73cb3
an-bhv <f20201892@hyderabad.bits-pilani.ac.in>
2021-09-06 00:14:33 +0530

    reset password done

--- pomo/main/views.py
+++ pomo/main/views.py
@@ -60,6 +60,7 @@ def logout_request(request):
 
 
 
+
 def password_reset_request(request):
 	if request.method == "POST":
 		password_reset_form = PasswordResetForm(request.POST)
@@ -75,7 +76,6 @@ def password_reset_request(request):
 					'domain':'127.0.0.1:8000',
 					'site_name': 'Website',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
-					"user": user,
 					'token': default_token_generator.make_token(user),
 					'protocol': 'http',
 					}
@@ -83,7 +83,10 @@ def password_reset_request(request):
 					try:
 						send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
 					except BadHeaderError:
+
 						return HttpResponse('Invalid header found.')
-					return redirect ("/password_reset/done/")
+						
+					messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
+					return redirect ("main:homepage")
 	password_reset_form = PasswordResetForm()
 	return render(request=request, template_name="main/password/password_reset.html", context={"password_reset_form":password_reset_form})
